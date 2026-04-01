c6117663b1548dd60e2431cfea0de67299ba08c0
Abhinav <abhinavsinghasb@gmail.com>
2026-04-02 00:17:56 +0530

    init commit

---
+++ src/main.cu
@@ -0,0 +1,18 @@
+// 1. Define he macros (do this in ONLY ONE file in your project)
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+// 2. Include the headers
+#include "../include/stb_image.h"
+#include "../include/stb_image_write.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "CUDA Augmentation Project Initialized." << std::endl;
+    std::cout << "stb_image loaded successfully." << std::endl;
+    
+    // We will put the image loading logic here next.
+    
+    return 0;
+}
